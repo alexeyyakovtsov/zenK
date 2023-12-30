0e0d0404ff3345fd2537042a61894a996afa072b
im-ayakovtsov <ayakovtsov@interfirst.com>
2023-12-30 18:56:43 +0100

    version rollback

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,7 @@
-# Используем базовый образ с JDK
-FROM openjdk:11-jre-slim as builder
-
-# Устанавливаем рабочую директорию
-WORKDIR /app
-
-# Копируем файлы Gradle Wrapper
-COPY gradlew .
-COPY gradle gradle
-
-# Копируем файлы с зависимостями
-COPY build.gradle .
-COPY settings.gradle .
-
-# Выполняем сборку, обновляем зависимости
-RUN ./gradlew build --no-daemon
-
-# Копируем остальные файлы проекта
-COPY . .
-
-# Снова выполняем сборку для финального JAR
-RUN ./gradlew build --no-daemon
-
-# Отдельный этап для минимизации образа
-FROM openjdk:11-jre-slim
-
-# Устанавливаем рабочую директорию
-WORKDIR /app
-
-# Копируем JAR-файл из предыдущего этапа
-COPY --from=builder /app/build/libs/backend-*.jar ./application.jar
-
-# Команда для запуска приложения
-CMD java -server -Xmx256M -jar application.jar
+FROM openjdk:11-jre-slim 
+ 
+WORKDIR root/ 
+ 
+ADD backend/build/libs/backend-*.jar ./application.jar 
+ 
+CMD java -server -Xmx256M -jar /root/application.jar
--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,28 +10,11 @@ pipeline {
     }
 
     stages {
-        stage('Checkout') {
-            steps {
-                script {
-                    checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[credentialsId: 'github-credentials-id', url: 'https://github.com/alexeyyakovtsov/zenK']]])
-                }
-            }
-        }
-
         stage('Build') {
             steps {
                 script {
-                    docker.build IMAGE_NAME, '-f Dockerfile .'
-                }
-            }
-        }
-
-        stage('Test') {
-            steps {
-                script {
-                    docker.image(IMAGE_NAME).inside {
-                        sh './gradlew test --no-daemon'
-                    }
+                    // Этот этап повторяет build-jar из вашего GitLab CI/CD
+                    sh 'docker build -t my-backend:latest -f Dockerfile .'
                 }
             }
         }
@@ -39,11 +22,11 @@ pipeline {
         stage('Package') {
             steps {
                 script {
+                    // Этот этап повторяет package-docker из вашего GitLab CI/CD
                     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials-id', usernameVariable: 'DOCKER_LOGIN', passwordVariable: 'DOCKER_PASS')]) {
-                        docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials-id') {
-                            sh "docker tag $IMAGE_NAME:latest $DOCKER_LOGIN/$IMAGE_NAME:latest"
-                            sh 'docker push $DOCKER_LOGIN/$IMAGE_NAME:latest'
-                        }
+                        sh 'docker login -u $DOCKER_LOGIN -p $DOCKER_PASS'
+                        sh 'docker build -t $IMAGE_NAME:latest .'
+                        sh 'docker push $IMAGE_NAME:latest'
                     }
                 }
             }
@@ -52,12 +35,13 @@ pipeline {
         stage('Deploy') {
             steps {
                 script {
+                    // Этот этап повторяет deploy из вашего GitLab CI/CD
                     withCredentials([sshUserPrivateKey(credentialsId: 'ssh-key-credentials-id', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USER')]) {
                         sh '''
                             ssh $SSH_USER@$SSH_HOST "docker rm -f $CONTAINER_NAME || true"
                             ssh $SSH_USER@$SSH_HOST "docker rmi $IMAGE_NAME || true"
                             ssh $SSH_USER@$SSH_HOST "docker login -u $DOCKER_LOGIN -p $DOCKER_PASS"
-                            ssh $SSH_USER@$SSH_HOST "docker pull $DOCKER_LOGIN/$IMAGE_NAME:latest"
+                            ssh $SSH_USER@$SSH_HOST "docker pull $IMAGE_NAME"
                             ssh $SSH_USER@$SSH_HOST "docker run -d --name $CONTAINER_NAME --restart always \
                                 --network kicker-net \
                                 -p $CONTAINER_PORT:8080 \
@@ -67,7 +51,7 @@ pipeline {
                                 -e POSTGRES_USER=$POSTGRES_USER \
                                 -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
                                 -e DOMAINS=$DOMAINS \
-                                $DOCKER_LOGIN/$IMAGE_NAME:latest"
+                                $IMAGE_NAME"
                         '''
                     }
                 }
