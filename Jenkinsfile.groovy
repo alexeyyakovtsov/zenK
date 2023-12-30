91b1b9fc923ca2f62d6e1feee7160444a2e4667f
im-ayakovtsov <ayakovtsov@interfirst.com>
2023-12-30 19:04:37 +0100

    update jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,11 +13,9 @@ pipeline {
         stage('Build') {
             steps {
                 script {
-                    container('openjdk:8-jdk') {
-                        script {
-                            sh 'export GRADLE_USER_HOME=`pwd`/.gradle'
-                            sh './gradlew build'
-                        }
+                    dir('backend') {
+                        sh 'export GRADLE_USER_HOME=`pwd`/.gradle'
+                        sh './gradlew build'
                     }
                 }
             }
@@ -28,10 +26,10 @@ pipeline {
                 script {
                     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials-id', usernameVariable: 'DOCKER_LOGIN', passwordVariable: 'DOCKER_PASS')]) {
                         script {
-                            if (env.CI_COMMIT_REF_NAME == 'master') {
+                            if (env.BRANCH_NAME == 'main') {
                                 env.IMAGE_TAG = 'latest'
                             } else {
-                                env.IMAGE_TAG = env.CI_COMMIT_REF_NAME
+                                env.IMAGE_TAG = env.BRANCH_NAME
                             }
                             sh 'docker login -u $DOCKER_LOGIN -p $DOCKER_PASS'
                             sh 'docker build -t $IMAGE_NAME:$IMAGE_TAG .'
@@ -50,7 +48,7 @@ pipeline {
                             ssh $SSH_USER@$SSH_HOST "docker rm -f $CONTAINER_NAME || true"
                             ssh $SSH_USER@$SSH_HOST "docker rmi $IMAGE_NAME || true"
                             ssh $SSH_USER@$SSH_HOST "docker login -u $DOCKER_LOGIN -p $DOCKER_PASS"
-                            ssh $SSH_USER@$SSH_HOST "docker pull $IMAGE_NAME"
+                            ssh $SSH_USER@$SSH_HOST "docker pull $IMAGE_NAME:$IMAGE_TAG"
                             ssh $SSH_USER@$SSH_HOST "
                                 docker run -d --name $CONTAINER_NAME --restart always \
                                   --network kicker-net \
@@ -61,7 +59,7 @@ pipeline {
                                   -e POSTGRES_USER=$POSTGRES_USER \
                                   -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
                                   -e DOMAINS=$DOMAINS \
-                                  $IMAGE_NAME
+                                  $IMAGE_NAME:$IMAGE_TAG
                             "
                         '''
                     }
