8d70c0d099bf6ea9125a8be7b8706487db87a5b9
im-ayakovtsov <ayakovtsov@interfirst.com>
2024-01-02 10:54:25 +0100

    change jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,22 +1,21 @@
 pipeline {
-    agent any
-    
+    agent {
+        docker {
+            image 'docker:latest'
+            args '-v /var/run/docker.sock:/var/run/docker.sock'
+        }
+    }
+
     environment {
         DOCKER_DRIVER = 'overlay2'
         IMAGE_NAME = 'zensoftio/kicker'
-        CONTAINER_NAME = 'kicker'
-        CONTAINER_PORT = '8585'
-        DOMAINS = 'zensoft.io,zensoft.by,zensoft.kg'
     }
 
     stages {
         stage('Build') {
             steps {
                 script {
-                    dir('backend') {
-                        sh 'export GRADLE_USER_HOME=`pwd`/.gradle'
-                        sh './gradlew build'
-                    }
+                    sh 'docker run --rm -v $PWD:/app -w /app openjdk:8-jdk ./gradlew build'
                 }
             }
         }
@@ -24,18 +23,12 @@ pipeline {
         stage('Package') {
             steps {
                 script {
-                    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials-id', usernameVariable: 'DOCKER_LOGIN', passwordVariable: 'DOCKER_PASS')]) {
-                        script {
-                            if (env.BRANCH_NAME == 'main') {
-                                env.IMAGE_TAG = 'latest'
-                            } else {
-                                env.IMAGE_TAG = env.BRANCH_NAME
-                            }
-                            sh 'docker login -u $DOCKER_LOGIN -p $DOCKER_PASS'
-                            sh 'docker build -t $IMAGE_NAME:$IMAGE_TAG .'
-                            sh 'docker push $IMAGE_NAME:$IMAGE_TAG'
-                        }
-                    }
+            def imageTag = env.BRANCH_NAME == 'master' ? 'latest' : env.BRANCH_NAME
+            withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials-id', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+                sh "docker login -u \$DOCKER_USERNAME -p \$DOCKER_PASSWORD"
+            }
+            sh "docker build -t \${IMAGE_NAME}:${imageTag} ."
+            sh "docker push \${IMAGE_NAME}:${imageTag}"
                 }
             }
         }
@@ -43,26 +36,33 @@ pipeline {
         stage('Deploy') {
             steps {
                 script {
-                    withCredentials([sshUserPrivateKey(credentialsId: 'ssh-key-credentials-id', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USER')]) {
-                        sh '''
-                            ssh $SSH_USER@$SSH_HOST "docker rm -f $CONTAINER_NAME || true"
-                            ssh $SSH_USER@$SSH_HOST "docker rmi $IMAGE_NAME || true"
-                            ssh $SSH_USER@$SSH_HOST "docker login -u $DOCKER_LOGIN -p $DOCKER_PASS"
-                            ssh $SSH_USER@$SSH_HOST "docker pull $IMAGE_NAME:$IMAGE_TAG"
-                            ssh $SSH_USER@$SSH_HOST "
-                                docker run -d --name $CONTAINER_NAME --restart always \
-                                  --network kicker-net \
-                                  -p $CONTAINER_PORT:8080 \
-                                  -v $DATA_DIR:/data/ \
-                                  -e POSTGRES_HOST=$POSTGRES_HOST \
-                                  -e POSTGRES_DB=$POSTGRES_DB \
-                                  -e POSTGRES_USER=$POSTGRES_USER \
-                                  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
-                                  -e DOMAINS=$DOMAINS \
-                                  $IMAGE_NAME:$IMAGE_TAG
-                            "
-                        '''
-                    }
+                    sh '''
+                        apk add --no-cache openssh-client
+                        eval $(ssh-agent -s)
+                        echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
+                        mkdir -p ~/.ssh
+                        chmod 700 ~/.ssh
+                        ssh-keyscan $SSH_HOST >> ~/.ssh/known_hosts
+                        chmod 644 ~/.ssh/known_hosts
+                    '''
+                    sh '''
+                        ssh $SSH_USER@$SSH_HOST "docker rm -f kicker || true"
+                        ssh $SSH_USER@$SSH_HOST "docker rmi $IMAGE_NAME || true"
+                        ssh $SSH_USER@$SSH_HOST "docker login -u \${DOCKER_LOGIN} -p \${DOCKER_PASS}"
+                        ssh $SSH_USER@$SSH_HOST "docker pull $IMAGE_NAME"
+                        ssh $SSH_USER@$SSH_HOST "
+                            docker run -d --name kicker --restart always \
+                            --network kicker-net \
+                            -p 8585:8080 \
+                            -v $DATA_DIR:/data/ \
+                            -e POSTGRES_HOST=$POSTGRES_HOST \
+                            -e POSTGRES_DB=$POSTGRES_DB \
+                            -e POSTGRES_USER=$POSTGRES_USER \
+                            -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+                            -e DOMAINS=zensoft.io,zensoft.by,zensoft.kg \
+                            $IMAGE_NAME
+                        "
+                    '''
                 }
             }
         }
