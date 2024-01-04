2762fc45e88f4a70d59ff5e88259379bc95f3956
im-ayakovtsov <ayakovtsov@interfirst.com>
2024-01-04 17:57:45 +0100

    jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,15 +17,15 @@ stages {
             }
         }
 
-        // stage('Build') {
-        //     steps {
-        //         script {
-        //             docker.image('openjdk:11-jdk').inside('-v /var/run/docker.sock:/var/run/docker.sock') {
-        //                 sh './gradlew build'
-        //             }
-        //         }
-        //     }
-        // }
+        stage('Build') {
+            steps {
+                script {
+                    docker.image('openjdk:11-jdk').inside('-v /var/run/docker.sock:/var/run/docker.sock') {
+                        sh './gradlew build'
+                    }
+                }
+            }
+        }
 
         stage('Package and Push to Docker Hub') {
             steps {
@@ -40,51 +40,51 @@ stages {
             }
         }
 
-        // stage('Deploy') {
-        //     steps {
-        //         script {
-        //             env.remote = [
-        //                 name: 'DeployServer',
-        //                 host: '172.31.45.29',
-        //                 user: 'ubuntu',
-        //                 keyFile: credentials('id_rsa')
-        //             ]
+        stage('Deploy') {
+            steps {
+                script {
+                    env.remote = [
+                        name: 'DeployServer',
+                        host: '172.31.45.29',
+                        user: 'ubuntu',
+                        keyFile: credentials('id_rsa')
+                    ]
 
-        //             sh '''
-        //                 apk add --no-cache openssh-client
-        //                 eval $(ssh-agent -s)
-        //                 echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
-        //                 mkdir -p ~/.ssh
-        //                 chmod 700 ~/.ssh
-        //                 ssh-keyscan $SSH_HOST >> ~/.ssh/known_hosts
-        //                 chmod 644 ~/.ssh/known_hosts
-        //             '''
-        //             sh '''
-        //                 ssh $SSH_USER@$SSH_HOST "docker rm -f kicker || true"
-        //                 ssh $SSH_USER@$SSH_HOST "docker rmi $IMAGE_NAME || true"
-        //                 ssh $SSH_USER@$SSH_HOST "docker pull $IMAGE_NAME"
-        //                 ssh $SSH_USER@$SSH_HOST "
-        //                     docker run -d --name kicker --restart always \
-        //                     --network kicker-net \
-        //                     -p 8585:8080 \
-        //                     -v $DATA_DIR:/data/ \
-        //                     -e POSTGRES_HOST=$POSTGRES_HOST \
-        //                     -e POSTGRES_DB=$POSTGRES_DB \
-        //                     -e POSTGRES_USER=$POSTGRES_USER \
-        //                     -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
-        //                     -e DOMAINS=zensoft.io,zensoft.by,zensoft.kg \
-        //                     $IMAGE_NAME
-        //                 "
-        //             '''
-        //         }
-        //         script {
-        //             sshCommand remote: env.remote,
-        //                 command: '''
-        //                     docker-compose -f docker-compose.yml up -d
-        //                 '''
-        //         }
-        //     }
-        // }
+                    sh '''
+                        apk add --no-cache openssh-client
+                        eval $(ssh-agent -s)
+                        echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
+                        mkdir -p ~/.ssh
+                        chmod 700 ~/.ssh
+                        ssh-keyscan $SSH_HOST >> ~/.ssh/known_hosts
+                        chmod 644 ~/.ssh/known_hosts
+                    '''
+                    sh '''
+                        ssh $SSH_USER@$SSH_HOST "docker rm -f kicker || true"
+                        ssh $SSH_USER@$SSH_HOST "docker rmi $IMAGE_NAME || true"
+                        ssh $SSH_USER@$SSH_HOST "docker pull $IMAGE_NAME"
+                        ssh $SSH_USER@$SSH_HOST "
+                            docker run -d --name kicker --restart always \
+                            --network kicker-net \
+                            -p 8585:8080 \
+                            -v $DATA_DIR:/data/ \
+                            -e POSTGRES_HOST=$POSTGRES_HOST \
+                            -e POSTGRES_DB=$POSTGRES_DB \
+                            -e POSTGRES_USER=$POSTGRES_USER \
+                            -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+                            -e DOMAINS=zensoft.io,zensoft.by,zensoft.kg \
+                            $IMAGE_NAME
+                        "
+                    '''
+                }
+                script {
+                    sshCommand remote: env.remote,
+                        command: '''
+                            docker-compose -f docker-compose.yml up -d
+                        '''
+                }
+            }
+        }
     }
 
     
