ae36d5589119966050cbcdedb56babaa64a8be26
im-ayakovtsov <ayakovtsov@interfirst.com>
2024-01-04 18:56:51 +0100

    jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -50,7 +50,7 @@ stages {
                         keyFile: credentials('id_rsa')
                     ]
                     sh '''
-                        apt-get update && apt-get install -y openssh-client
+                        sudo apt-get update && sudo apt-get install -y openssh-client
                         eval $(ssh-agent -s)
                         echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
                         mkdir -p ~/.ssh
